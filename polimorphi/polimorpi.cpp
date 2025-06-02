4defb64806e93ec58d954b54a6ccd306ac1fb22f
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-06-02 11:17:48 +0700

    main funcation

--- polimorphi/polimorpi.cpp
+++ polimorphi/polimorpi.cpp
@@ -16,10 +16,25 @@ class joko :public seseorang {
     }
 };
 
-class liaa :public seseorang {
+class lia :public seseorang {
     public:
     void pesan() {
         cout << "pesan dari lia" << endl;
     }
 
 };
+
+int main() {
+    seseorang* obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
+    obyek->pesan();
+
+
+
+    return 0;
+}
