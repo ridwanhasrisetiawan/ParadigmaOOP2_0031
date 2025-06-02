8827e819cb2e1a68db4eb400134aa3ff515ef31f
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-06-02 10:48:22 +0700

    membuat main funcation

--- noOverriding/noOverriding.cpp
+++ noOverriding/noOverriding.cpp
@@ -13,3 +13,10 @@ class deviredClass : public baseClass {
         cout << "Hallo saya Funcation dari devired Class";
     }
 };
+int main() {
+    deviredClass a;
+
+    a.perkenalan();
+
+    return 0;
+}
