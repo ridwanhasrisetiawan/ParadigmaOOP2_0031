e4e0457490054afc95210d2a123eeb4b059a3a15
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-06-02 10:36:43 +0700

    tambah kelas budi dedngan multiple inheritance

--- multiwaris/multiwaris.cpp
+++ multiwaris/multiwaris.cpp
@@ -31,3 +31,15 @@ class pelajar : virtual public orang {
         cout << "pelajar dibuat\n" << endl;
     }
 };
+
+class budi : public pekerja, public pelajar {
+    public:
+
+    budi(int pUmur) :
+    pekerja(pUmur),
+    pelajar(pUmur),
+    orang(pUmur)
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+};
