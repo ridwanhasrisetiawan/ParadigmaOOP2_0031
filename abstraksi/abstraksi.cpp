0f722f7b112d13a4e807f9ce96987c310b1bcc9f
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-06-02 10:03:49 +0700

    implementasi abstraksi kelas

--- abstraksi/abstraksi.cpp
+++ abstraksi/abstraksi.cpp
@@ -2,20 +2,29 @@
 
 using namespace std;
 
-class AbstraksiKlas {
-private: string x, y;
+class AbstraksiKlas
+{
+private:
+    string x, y;
 
 public:
+    void setXY(string a, string b)
+    {
+        x = a;
+        y = b;
+    }
 
-
-
-void setXY(string a, string b) {
-    x = a;
-    y = b;
-}
-
-void displa() {
-    cout << "x = " << x << endl;
-    cout << "y = " << y << endl;
-}
+    void display()
+    {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
 };
+
+int main() {
+    AbstraksiKlas ak;
+    ak.setXY("Yogyakarta", "Kampus");
+    ak.display();
+
+    return 0;
+}
